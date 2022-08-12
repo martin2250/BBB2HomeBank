28ab9bbc64aa7f1a163530448b83bb0c3b1d94aa
Martin Pittermann <martinp2250@gmail.com>
2022-08-12 11:10:55 +0200

    deduplicate lines

--- convert_csv.py
+++ convert_csv.py
@@ -7,7 +7,7 @@ import argparse
 parser = argparse.ArgumentParser(
     description='convert BBBank csv exports to HomeBank csv')
 
-parser.add_argument('file',
+parser.add_argument('files', nargs='+',
                     help='input BBBank csv')
 parser.add_argument('-o', '--output',
                     help='output homebank csv')
@@ -40,49 +40,61 @@ f_out = sys.stdout
 if args.output:
     f_out = open(args.output, 'w')
 
-reader = csv.reader(open(args.file, encoding='iso-8859-1'), delimiter=';')
+lines_out = set()
 
-# search for column headers
-for line in reader:
-    if not line or not any(line):
-        continue
-    if 'Buchungstag' in line:
-        break
+for file in args.files:
+    reader = csv.reader(open(file, encoding='iso-8859-1'), delimiter=';')
 
-# get indices by column headers
-i_date = line.index('Buchungstag')
-i_info = line.index('Vorgang/Verwendungszweck')
-i_payee = line.index('Zahlungsempfänger')
-i_amount = line.index('Umsatz')
-i_sh = line.index('Soll/Haben')
+    # search for column headers
+    for line in reader:
+        if not line or not any(line):
+            continue
+        if 'Buchungstag' in line:
+            break
+    else:
+        raise ValueError('end of header not found')
 
-# read transactions
-for line in reader:
-    if not line or not any(line):
-        continue
-    if 'Anfangssaldo' in line or 'Endsaldo' in line:
-        continue
-    date = line[i_date]
-    info = line[i_info].replace('\n', ' ')
-    payee = line[i_payee]
-    amount = float(line[i_amount].replace('.', '').replace(',', '.'))
-    if line[i_sh] == 'S':
-        amount *= -1
+    print(line)
+    # get indices by column headers
+    i_date = line.index('Buchungstag')
+    i_info = line.index('Vorgang/Verwendungszweck')
+    i_payee = line.index('Zahlungsempfänger')
+    i_amount = line.index('Umsatz')
+    i_sh = line.index('Soll/Haben')
 
-    if args.verbose:
-        print('-'*80, file=sys.stderr)
-        print('date', date, file=sys.stderr)
-        print('info', info, file=sys.stderr)
-        print('payee', payee, file=sys.stderr)
-        print('amount', amount, file=sys.stderr)
+    # read transactions
+    for line in reader:
+        if not line or not any(line):
+            continue
+        if 'Anfangssaldo' in line or 'Endsaldo' in line:
+            continue
+        date = line[i_date]
+        info = line[i_info].replace('\n', ' ')
+        payee = line[i_payee]
+        amount = float(line[i_amount].replace('.', '').replace(',', '.'))
+        if line[i_sh] == 'S':
+            amount *= -1
 
-    print(';'.join([
-        date,
-        '0',  # payment
-        info,
-        payee,
-        '',  # memo
-        str(amount),
-        '',  # category
-        '',  # tags
-    ]), file=f_out)
+        if args.verbose:
+            print('-'*80, file=sys.stderr)
+            print('date', date, file=sys.stderr)
+            print('info', info, file=sys.stderr)
+            print('payee', payee, file=sys.stderr)
+            print('amount', amount, file=sys.stderr)
+
+        lines_out.add(';'.join([
+            date,
+            '0',  # payment
+            info,
+            payee,
+            '',  # memo
+            str(amount),
+            '',  # category
+            '',  # tags
+        ]))
+
+lines_out = list(lines_out)
+lines_out.sort()
+
+for l in lines_out:
+    print(l, file=f_out)
